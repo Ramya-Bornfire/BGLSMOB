53cc2aaa3e02c1ac6580bc826ff059a961db8e1f
Sudeep-04 <sudeephebsur@bornfire.co.in>
2026-05-12 10:56:07 +0530

    LoanMaster Updated UI

--- BGLS2/app/src/main/java/com/example/bgls/LoanMaster/LoanMasterListActivity.kt
+++ BGLS2/app/src/main/java/com/example/bgls/LoanMaster/LoanMasterListActivity.kt
@@ -44,7 +44,13 @@ class LoanMasterListActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_loan_master_list)
         btnBack = findViewById(R.id.btnBack)
-        btnBack.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        btnBack.setOnClickListener { finish() }
+
+        findViewById<ImageView>(R.id.btnHome).setOnClickListener {
+            val hIntent = Intent(this, com.example.bgls.MainActivity::class.java)
+            hIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            startActivity(hIntent)
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
